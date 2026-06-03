a231fd14e1f27e533758eee9318e03c65987ab56
Zirong Wang <oscarwangartist@gmail.com>
2026-06-03 15:32:16 +0800

    commit

--- src/works.js
+++ src/works.js
@@ -67,8 +67,8 @@ export const works = [
     subtitle: '3D場景渲染圖 · 2026',
     year: '2026',
     image: '/works/Untitled.JPEG',
-    width: 198,
-    height: 280,
+    width: 192,
+    height: 108,
     description: '這是第六件作品的介紹文字。',
   },
   {
